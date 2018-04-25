70dba2b4d91e2f69dcc21c03ea77f43735d99570
rparimala07 <99seagull@gmail.com>
2018-04-24 21:55:20 -0400

    Changed port numbers from 8080 to 8888

--- client/src/app/shared/beer/beer.service.ts
+++ client/src/app/shared/beer/beer.service.ts
@@ -8,6 +8,6 @@ export class BeerService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get('http://localhost:8080/good-beers');
+    return this.http.get('http://localhost:8888/good-beers');
   }
 }
